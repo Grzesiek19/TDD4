31901812385a4ec36fd38048bd8952de36209297
Grzesiek19 <grzesyou@wp.pl>
2015-06-08 21:09:13 +0200

    Passed test for getting term No1

--- FibonacciTDD2/src/com/TDD/example/Fibonacci.java
+++ FibonacciTDD2/src/com/TDD/example/Fibonacci.java
@@ -2,8 +2,13 @@ package com.TDD.example;
 
 public class Fibonacci {
 
-	public int getFiboValue(int i) {
-		return 0;
+	public int getFiboValue(int term) {
+		int fibonacci = 0;
+		if (term == 0)
+			return 0;
+		else if (term == 1)
+			return 1;
+		return fibonacci;
 	}
 
 }
