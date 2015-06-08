4a63dd88fc6ecfb55ef6df4510037c7cc99ecb71
Grzesiek19 <grzesyou@wp.pl>
2015-06-08 20:55:32 +0200

    Passed test for getting term No0 of Fibonacci Sequence

--- FibonacciTDD2/src/com/TDD/example/Fibonacci.java
+++ FibonacciTDD2/src/com/TDD/example/Fibonacci.java
@@ -3,7 +3,7 @@ package com.TDD.example;
 public class Fibonacci {
 
 	public int getFiboValue(int i) {
-
+		return 0;
 	}
 
 }
--- FibonacciTDD2/src/com/TDD/example/FibonacciTest.java
+++ FibonacciTDD2/src/com/TDD/example/FibonacciTest.java
@@ -13,4 +13,10 @@ public class FibonacciTest {
 		assertEquals(0, fiboValueFor_0);
 	}
 
+	@Test
+	public void testForResultFiboTermFor_1() {
+		Fibonacci fibo = new Fibonacci();
+		int fiboValueFor_1 = fibo.getFiboValue(1);
+		assertEquals(1, fiboValueFor_1);
+	}
 }
