2ad3faf6de3d19fb9c5c997f38b7f61b123ac659
Grzesiek19 <grzesyou@wp.pl>
2015-06-08 21:11:06 +0200

    Failed test for getting term No5

--- FibonacciTDD2/src/com/TDD/example/FibonacciTest.java
+++ FibonacciTDD2/src/com/TDD/example/FibonacciTest.java
@@ -19,4 +19,11 @@ public class FibonacciTest {
 		int fiboValueFor_1 = fibo.getFiboValue(1);
 		assertEquals(1, fiboValueFor_1);
 	}
+
+	@Test
+	public void testForResultFiboTermFor_5() {
+		Fibonacci fibo = new Fibonacci();
+		int fiboValueFor_5 = fibo.getFiboValue(5);
+		assertEquals(5, fiboValueFor_5);
+	}
 }
